c37dccb9a898f4cbb1569c3a5b5ddf51e6b0f582
Петр Масляный <tangompp@gmail.com>
2020-10-29 02:50:40 +0300

    Update searchform.js

--- js/searchform.js
+++ js/searchform.js
@@ -8,41 +8,45 @@ const adults = document.querySelector(".adults");
 const kids = document.querySelector(".kids");
 
 openButtonForm.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    arrivalDate.focus();
-    adults.value = localStorage.getItem("adults");
-    kids.value = localStorage.getItem("kids", kids.value);
-    if (searchFormContainer.classList.contains("search-form-up")) {
-        searchFormContainer.classList.toggle("search-form-up");
-        searchFormContainer.classList.toggle("search-form-down");
-        searchFormContainer.classList.remove("search-form-error");
-    } else {
-        searchFormContainer.classList.remove("search-form-down");
-        searchFormContainer.classList.remove("search-form-alterror");
-        searchFormContainer.classList.add("search-form-up");
-    }
-    
+  evt.preventDefault();
+  arrivalDate.focus();
+  adults.value = localStorage.getItem("adults");
+  kids.value = localStorage.getItem("kids", kids.value);
+  if (searchFormContainer.classList.contains("search-form-up")) {
+    searchFormContainer.classList.toggle("search-form-up");
+    searchFormContainer.classList.toggle("search-form-down");
+    searchFormContainer.classList.remove("search-form-error");
+  } else {
+    searchFormContainer.classList.remove("search-form-down");
+    searchFormContainer.classList.remove("search-form-alterror");
+    searchFormContainer.classList.add("search-form-up");
+  }
 });
 
 searchForm.addEventListener("submit", function (evt) {
-    if (!arrivalDate.value || !departureDate.value || !adults.value || !kids.value) {
-        evt.preventDefault();
-        if (searchFormContainer.classList.contains("search-form-error")) {
-            searchFormContainer.classList.toggle("search-form-error");
-            searchFormContainer.classList.toggle("search-form-alterror");
-            searchFormContainer.classList.remove("search-form-down");
-        } else {
-            searchFormContainer.classList.add("search-form-error");
-            searchFormContainer.classList.remove("search-form-alterror");
-            searchFormContainer.classList.remove("search-form-down");   
-        } 
+  if (
+    !arrivalDate.value ||
+    !departureDate.value ||
+    !adults.value ||
+    !kids.value
+  ) {
+    evt.preventDefault();
+    if (searchFormContainer.classList.contains("search-form-error")) {
+      searchFormContainer.classList.toggle("search-form-error");
+      searchFormContainer.classList.toggle("search-form-alterror");
+      searchFormContainer.classList.remove("search-form-down");
     } else {
-        localStorage.setItem("adults", adults.value);
-        localStorage.setItem("kids", kids.value);
+      searchFormContainer.classList.add("search-form-error");
+      searchFormContainer.classList.remove("search-form-alterror");
+      searchFormContainer.classList.remove("search-form-down");
     }
+  } else {
+    localStorage.setItem("adults", adults.value);
+    localStorage.setItem("kids", kids.value);
+  }
 });
 
-/* 
+/*
 let isStorageSupport = true;
 let storage = "";
 
